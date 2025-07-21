ff37ce3d809516006923611a525f5afc704938ec
Agege88 <100889215+Agege88@users.noreply.github.com>
2025-07-21 14:21:38 -0700

    Fix Frontend Preview and Setup

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
